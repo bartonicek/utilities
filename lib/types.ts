80556b2e122056c600c99cdde50a42895c67aad8
Adam Bartonicek <bartonicek@gmail.com>
2023-11-15 14:47:11 +0100

    Minor fix

--- lib/types.ts
+++ lib/types.ts
@@ -1,10 +1,9 @@
 export type Key = string | number | symbol;
 export type Callback = (...args: any) => any;
-export type SideEffect = (...args: any) => void;
+export type SideEffect = () => void;
+export type Stringable = { toString(): string };
+export type Dict = Record<Key, any>;
 
 export type Lazy<T> = () => T;
 export type MapFn<T, U> = (next: T) => U;
 export type ReduceFn<T, U> = (prev: U, next: T) => U;
-
-export type Stringable = { toString(): string };
-export type Dict = Record<Key, any>;
